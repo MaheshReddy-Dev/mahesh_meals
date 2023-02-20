011da6e9abe3526ec6891e48b1a0cc71c493ec53
Mahesh Reddy <mahesh@singular.dev>
2023-02-20 18:03:58 +0530

    Added validations

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,3 +1,4 @@
 class Meal < ApplicationRecord
     has_many :meal_recipes ,  dependent: :destroy
+    validates :name, presence: true
 end
--- app/models/recipe_origin.rb
+++ app/models/recipe_origin.rb
@@ -1,3 +1,4 @@
 class RecipeOrigin < ApplicationRecord
   belongs_to :meal_recipe
+  validates :name, presence: true
 end
